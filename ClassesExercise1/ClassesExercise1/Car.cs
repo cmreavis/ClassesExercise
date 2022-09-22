22843f6769c828ae74ffbaf7f72a682fabb6096c
cmreavis <cmreavis97@gmail.com>
2022-09-22 11:16:52 -0400

    First commit

---
+++ ClassesExercise1/ClassesExercise1/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise1
+{
+    internal class Car
+    {
+        public void PrintCarInfo()
+        {
+            Console.WriteLine($"Model Year: {Year} ");
+            Console.WriteLine($"Make: {Make} ");
+            Console.WriteLine($"Model: {Model} ");
+            Console.WriteLine();
+            Console.WriteLine($"{Slogan}");
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string ?Slogan { get; set; }
+    }
+}
---
+++ ClassesExercise1/ClassesExercise1/Program.cs
@@ -0,0 +1,7 @@
+﻿using ClassesExercise1;
+
+Car newCar = new Car() { Make = "Fjord" , Model = "Fjiesta" , Year = 2000};
+newCar.Slogan = "Built Fjord Tough.";
+
+newCar.PrintCarInfo();
+
